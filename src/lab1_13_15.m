e974af612a0fe75ec2f93e0e4d7929f328f672bb
Owen Smith <ocsmith@wpi.edu>
2018-09-04 17:37:05 -0400

    Setpoints work

--- src/lab1_13_15.m
+++ src/lab1_13_15.m
@@ -32,18 +32,19 @@ myHIDSimplePacketComs.connect();
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = PacketProcessor(myHIDSimplePacketComs); 
 try
-  SERV_ID = 37;            % we will be talking to server ID 37 on
+  PID_SERV_ID = 37;            % we will be talking to server ID 37 on
                            % the Nucleo
                            
   STAT_SERV_ID = 21;       % when passing this ID talk to the status server
                            
-  CALIB_SERV_ID = 25;      % when passing this ID talk to the calibration
+  CALIB_SERV_ID = 25;      % when passing this ID talk to the calibration10
                            % server
   
-myHIDSimplePacketComs.setVid(vid);
+  myHIDSimplePacketComs.setVid(vid);
 
   DEBUG   = true;          % enables/disables debug prints
-
+  needsCalibrate = true;   %needs to calibrate?
+  
   % Instantiate a packet - the following instruction allocates 64
   % bytes for this purpose. Recall that the HID interface supports
   % packet sizes up to 64 bytes.
@@ -52,19 +53,15 @@ myHIDSimplePacketComs.setVid(vid);
   % The following code generates a sinusoidal trajectory to be
   % executed on joint 1 of the arm and iteratively sends the list of
   % setpoints to the Nucleo firmware.
-  
-  viaPtsAngles = [[0; 0; 0], [40; 45; 50], [0; 0; 0]];
+  0
+  viaPtsAngles = [[0; 5; 0], [-45; 45; 45], [0; 5; 0]];
   viaPts = [[0; 0; 0],[0; 0; 0],[0; 0; 0]];
   viaPts = viaPtsAngles * 1000 / 90;
   
-  Kp=1;
-  Ki=1;
-  Kd=1;
+  % C = pid(Kp,Ki,Kd)
   
-  C = pid(Kp,Ki,Kd)
-  
-%   disp(viaPtsAngles)
-%   disp(viaPts)
+  %   disp(viaPtsAngles)
+  %   disp(viaPts)
   d = size(viaPtsAngles);
   
   % The following definitions declare matrices
@@ -80,33 +77,33 @@ myHIDSimplePacketComs.setVid(vid);
 
   % Iterate through points given
   for k = viaPts
-%       disp('K Value')
-%       disp(k)
-      tic
-      %incremtal = (single(k) / sinWaveInc);
-      packet = zeros(15, 1, 'single');
-      packet(1:3) = k;
+    if needsCalibrate
+        for n = test 
+%           disp('K Value')
+%           disp(k)
+            tic
+      %     incremtal = (single(k) / sinWaveInc);
+            packet = zeros(15, 1, 'single');
+            packet(1) = n;
       
-      %Send packet to the server and get the response
+      %     Send packet to the server and get the response
             returnPacket = pp.command(STAT_SERV_ID, packet);
             
-      % Sets the received packet into a 3x3 matrix
-      returnPacketMatrix = [returnPacket(1,1) returnPacket(2,1) returnPacket(3,1);
-                             returnPacket(4,1) returnPacket(5,1) returnPacket(6,1);
-                             returnPacket(7,1) returnPacket(8,1) returnPacket(9,1)];
+      %     Sets the received packet viaPts(k)into a 3x3 matrix
+            returnPacketMatrix = [returnPacket(1,1) returnPacket(2,1) returnPacket(3,1);
+                                  returnPacket(4,1) returnPacket(5,1) returnPacket(6,1);
+                                  returnPacket(7,1) returnPacket(8,1) returnPacket(9,1)];
 
-      if DEBUG
-          disp('Sent Packet:');
-          disp(packet);
-          disp('Received Packet:');
-          disp(returnPacketMatrix);
-          
-      end
+            if DEBUG
+          %     disp('Sent Packet:');
+          %     disp(packet);
+                disp('Received Packet:');
+                disp(returnPacketMatrix);
+            end
       
-            
-%       if k > 1 
-%           positions(:,:,k-1) = returnPacketMatrix;
-%       end
+            if n > 1 
+                positions(:,:,n-1) = returnPacketMatrix;
+            end
       
 %       for x = 0:3
 %           packet((x*3)+1)=0.1;
@@ -119,42 +116,45 @@ myHIDSimplePacketComs.setVid(vid);
 %           disp('Received Packet 2:');
 %           disp(returnPacket2);
 %       end
-      toc
-      pause(1) %timeit(returnPacket) !FIXME why is this needed?
-      
-      
-  end
+            toc
+            pause(1) %timeit(returnPacket) !FIXME why is this needed?
+        end
   
-  % Averages the layers of the 3D matrix
-  average = ((positions(:,:,1) + positions(:,:,2) + positions(:,:,3))/3);
-  % Sets the home position as the first column of the averaged layers
-  home = average(:,1);
+  %         Averages the layers of the 3D matrix
+            average = ((positions(:,:,1) + positions(:,:,2) + positions(:,:,3))/3);
+  %         Sets the home position as the first column of the averaged layers
+            home = average(:,1);
   
-  disp('Final')
-  disp(positions);
-   %csvwrite(['lab1Q7_' datestr(now,'mmddyyHHMMSS')  '.csv'],positions);
-  disp('Average')
-  disp(average)
-  disp('Home')
-  disp(home)
+            disp('Final')
+            disp(positions);
+   %        csvwrite(['lab1Q7_' datestr(now,'mmddyyHHMMSS')  '.csv'],positions);
+            disp('Average')
+            disp(average)
+            disp('Home')
+            disp(home)
   
-  % sends the new home packet to the calibration server
-   pp.write(CALIB_SERV_ID,home)
+  %         sends the new home packet to the calibration server
+            pp.write(CALIB_SERV_ID,home)
  
  
- toc
-   % gets the position from the status server to determine whether the
-   % position reflects the new home
-   pause(1);
-   newHome = pp.command(STAT_SERV_ID, packet);
+            toc
+            pause(1);
+   %        gets the position from the status server to determine whether the
+   %        position reflects the new home
+        needsCalibrate = false;
+    end
+    
+    pidPacket = zeros(1, 15, 'single');
+    pidPacket(1:3) = k;
+    pp.write(PID_SERV_ID, pidPacket);
+    pause(.004);
+    pidReturnPacket = pp.read(PID_SERV_ID);
    
-   %converts the received packet to a matrix
-   newHomeMatrix = [newHome(1,1) newHome(2,1) newHome(3,1);
-                    newHome(4,1) newHome(5,1) newHome(6,1);
-                    newHome(7,1) newHome(8,1) newHome(9,1)];
-   disp('New Home')
-   disp(newHomeMatrix)
-  
+    disp("PID");
+    disp(pidPacket);
+   
+    pause(2);
+  end
   
 catch exception
     getReport(exception)
